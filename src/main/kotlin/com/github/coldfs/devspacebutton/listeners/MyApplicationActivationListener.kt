68079575a6c946e9835ef38da98d7bb53b7d5ef7
GitHub Action <action@github.com>
2025-02-13 16:33:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/coldfs/devspacebutton/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/coldfs/devspacebutton/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.coldfs.devspacebutton.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
