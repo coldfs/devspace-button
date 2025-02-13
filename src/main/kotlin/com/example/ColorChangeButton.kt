aa238b4802e53ced910f03d8f66285dca47d8fe2
Sergey Ivanov <ivanov.s@skyeng.ru>
2025-02-13 20:12:06 +0500

    just button runnable

---
+++ src/main/kotlin/com/example/ColorChangeButton.kt
@@ -0,0 +1,35 @@
+package com.example
+
+import com.intellij.openapi.wm.CustomStatusBarWidget
+import com.intellij.openapi.wm.StatusBar
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JComponent
+import org.jetbrains.annotations.NotNull
+
+class ColorChangeButton : CustomStatusBarWidget {
+    private val button = JButton().apply {
+        background = Color.RED
+        preferredSize = Dimension(20, 20)
+    }
+    
+    private var isYellow = false
+    
+    init {
+        button.addActionListener {
+            isYellow = !isYellow
+            button.background = if (isYellow) Color.YELLOW else Color.RED
+        }
+    }
+
+    @NotNull
+    override fun ID(): String = "ColorChangeButton"
+
+    @NotNull
+    override fun getComponent(): JComponent = button
+
+    override fun install(statusBar: StatusBar) {}
+
+    override fun dispose() {}
+} 
---
+++ src/main/kotlin/com/example/ColorChangeButtonFactory.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+
+class ColorChangeButtonFactory : StatusBarWidgetFactory {
+    override fun getId(): String = "ColorChangeButton"
+
+    override fun getDisplayName(): String = "Color Change Button"
+
+    override fun isAvailable(project: Project): Boolean = true
+
+    override fun createWidget(project: Project): StatusBarWidget = ColorChangeButton()
+
+    override fun disposeWidget(widget: StatusBarWidget) {}
+} 
